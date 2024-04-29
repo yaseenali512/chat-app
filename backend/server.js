602241b2026c24f00f1cefe78e97462e4a2a2d89
Your Name <you@example.com>
2024-04-29 12:28:59 +0500

    backend part completed

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log("Server is running on port", PORT);
+});
